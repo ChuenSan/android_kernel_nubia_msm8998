34202d032d1abae3f922598b3d2f045c2beb2518
ChuenSan <109579119+ChuenSan@users.noreply.github.com>
2024-10-28 21:35:14 +0800

    Update fair.c

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -31,7 +31,6 @@
 #include <linux/migrate.h>
 #include <linux/task_work.h>
 #include <linux/module.h>
-#include <linux/sched/sched.h>  // 添加此行，确保SCHED_WARN_ON声明可用
 
 #include "sched.h"
 #include <trace/events/sched.h>
